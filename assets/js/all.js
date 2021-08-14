3f706417ff4193035d21c2ee6f1d36d93a47f46b
valerie300 <zxcv841219595@gmail.com>
2021-08-14 20:49:12 +0800

    Update 2021-08-14T12:49:02.502Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,10 +1,13 @@
-$(function() {
+"use strict";
+
+$(function () {
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
-      clickable: true,
-    },
+      clickable: true
+    }
   });
 });
+//# sourceMappingURL=all.js.map
