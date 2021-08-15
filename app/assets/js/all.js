303a7e7af6b10742f168dee3251fa457ded856a4
valerie300 <zxcv841219595@gmail.com>
2021-08-15 15:01:47 +0800

    git init

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,10 +1,22 @@
-$(function() {
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+   // Responsive breakpoints
+   breakpoints: {
+    // when window width is >= 375px
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 20
     },
-  });
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 1110px
+    1110: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
